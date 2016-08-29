259c459a1b516a518430e405ca83674e684bec0a
Min Cai <min.cai.china@gmail.com>
2016-08-29 21:00:59 +0800

    Add cache_access.go, cache_replacement_policy.go, cache_replacement_policy_lru.go and simple_cache.go.

--- cpu/uncore/cache_replacement_policy.go
+++ cpu/uncore/cache_replacement_policy.go
@@ -1,30 +1,5 @@
 package uncore
 
-type CacheAccess struct {
-	Cache *Cache
-	Access *MemoryHierarchyAccess
-	Set                   uint32
-	Way                   uint32
-	Line                  *CacheLine
-	HitInCache            bool
-	Replacement           bool
-}
-
-func NewCacheAccess(cache *Cache, access *MemoryHierarchyAccess, set uint32, way uint32, tag uint32) *CacheAccess {
-	var cacheAccess = &CacheAccess{
-		Cache:cache,
-		Access:access,
-		Set:set,
-		Way:way,
-		Line:cache.Sets[set].Lines[way],
-	}
-
-	cacheAccess.HitInCache = cacheAccess.Line.Tag == int32(tag)
-	cacheAccess.Replacement = cacheAccess.Line.Valid()
-
-	return cacheAccess
-}
-
 type CacheReplacementPolicy interface {
 	Cache() *Cache
 	NewMiss(access *MemoryHierarchyAccess, set uint32, address uint32) *CacheAccess
