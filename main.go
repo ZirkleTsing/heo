f1318787620f39b7f5fc5ce1156f5992030f979f
Min Cai <min.cai.china@gmail.com>
2016-09-22 14:43:37 +0800

    Update test results.

--- main.go
+++ main.go
@@ -8,8 +8,8 @@ var (
 	numCores = int32(2)
 	numThreadsPerCore = int32(2)
 
-	maxDynamicInsts = int64(1)
-	fastForwardDynamicInsts = int64(100000000)
+	maxDynamicInsts = int64(10000000)
+	fastForwardDynamicInsts = int64(200000000)
 )
 
 func main() {
@@ -21,7 +21,7 @@ func mstBaseline() {
 	var config = cpu.NewCPUConfig("test_results/real/mst_baseline")
 
 	config.ContextMappings = append(config.ContextMappings,
-		cpu.NewContextMapping(0, "/home/itecgo/Projects/Archimulator/benchmarks/Olden_Custom1/mst/baseline/mst.mips", "400"))
+		cpu.NewContextMapping(0, "/home/itecgo/Projects/Archimulator/benchmarks/Olden_Custom1/mst/baseline/mst.mips", "1000"))
 
 	config.NumCores = numCores
 	config.NumThreadsPerCore = numThreadsPerCore
@@ -37,7 +37,7 @@ func mstHelperThreaded() {
 	var config = cpu.NewCPUConfig("test_results/real/mst_ht")
 
 	config.ContextMappings = append(config.ContextMappings,
-		cpu.NewContextMapping(0, "/home/itecgo/Projects/Archimulator/benchmarks/Olden_Custom1/mst/ht/mst.mips", "400"))
+		cpu.NewContextMapping(0, "/home/itecgo/Projects/Archimulator/benchmarks/Olden_Custom1/mst/ht/mst.mips", "1000"))
 
 	config.NumCores = numCores
 	config.NumThreadsPerCore = numThreadsPerCore
