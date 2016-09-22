3f60098e4ebb468e924ec2431d78207f56bb43a0
Min Cai <min.cai.china@gmail.com>
2016-09-22 12:47:24 +0800

    Add mst baseline in test results.

--- main.go
+++ main.go
@@ -5,6 +5,27 @@ import (
 )
 
 func main() {
+	mstBaseline()
+	mstHelperThreaded()
+}
+
+func mstBaseline() {
+	var config = cpu.NewCPUConfig("test_results/real/mst_baseline_100")
+
+	config.ContextMappings = append(config.ContextMappings,
+		cpu.NewContextMapping(0, "/home/itecgo/Projects/Archimulator/benchmarks/Olden_Custom1/mst/baseline/mst.mips", "100"))
+
+	config.NumCores = 2
+	config.NumThreadsPerCore = 2
+	config.MaxDynamicInsts = 1
+	config.FastForwardDynamicInsts = 100000000
+
+	var experiment = cpu.NewCPUExperiment(config)
+
+	experiment.Run(false)
+}
+
+func mstHelperThreaded() {
 	var config = cpu.NewCPUConfig("test_results/real/mst_ht_100")
 
 	config.ContextMappings = append(config.ContextMappings,
@@ -16,13 +37,6 @@ func main() {
 	config.FastForwardDynamicInsts = 100000000
 
 	var experiment = cpu.NewCPUExperiment(config)
-
-	//experiment.BlockingEventDispatcher().AddListener(reflect.TypeOf((*cpu.StaticInstExecutedEvent)(nil)), func(event interface{}) {
-	//	var staticInstExecutedEvent = event.(*cpu.StaticInstExecutedEvent)
-	//	fmt.Printf("[thread#%d] %s\n", staticInstExecutedEvent.Context.ThreadId, staticInstExecutedEvent.StaticInst.Disassemble(staticInstExecutedEvent.Pc))
-	//	fmt.Printf("#dynamicInsts: %d\n", experiment.Processor.Cores[0].Threads()[0].NumDynamicInsts)
-	//	fmt.Println(staticInstExecutedEvent.Context.Regs.Dump())
-	//})
 
 	experiment.Run(false)
 }
