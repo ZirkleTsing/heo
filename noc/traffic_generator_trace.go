5cfadc3b67d1028e4fa32793e9d47aecd00b476d
Min Cai <min.cai.china@bjut.edu.cn>
2017-05-11 15:25:59 +0800

    Update traffic_generator_trace.go and main.go.

--- noc/traffic_generator_trace.go
+++ noc/traffic_generator_trace.go
@@ -4,7 +4,6 @@ import (
 	"os"
 	"log"
 	"bufio"
-	"fmt"
 	"strings"
 	"strconv"
 )
@@ -22,7 +21,7 @@ type TraceTrafficGenerator struct {
 	MaxPackets           int64
 	TraceFileNames       []string
 	TraceFileLines       [][]*TraceFileLine
-	CurrentTraceFileLine []int64
+	CurrentTraceFileLine []int
 }
 
 func NewTraceTrafficGenerator(network *Network, packetInjectionRate float64, maxPackets int64, traceFileNames []string) *TraceTrafficGenerator {
@@ -43,8 +42,6 @@ func NewTraceTrafficGenerator(network *Network, packetInjectionRate float64, max
 
 		scanner := bufio.NewScanner(traceFile)
 		for scanner.Scan() {
-			fmt.Println(scanner.Text())
-
 			var line = scanner.Text()
 			var parts = strings.Split(line, ",")
 
@@ -75,18 +72,28 @@ func NewTraceTrafficGenerator(network *Network, packetInjectionRate float64, max
 		}
 
 		traceFile.Close()
+
+		generator.CurrentTraceFileLine = append(generator.CurrentTraceFileLine, 0)
 	}
 
 	return generator
 }
 
 func (generator *TraceTrafficGenerator) AdvanceOneCycle() {
-	//TODO：每隔10个时钟周期（cycle），从每个trace文件中取一行，并创建和发送对应的消息包（packet)
-	if(generator.Network.Driver.CycleAccurateEventQueue().CurrentCycle % 10 == 0) {
-		var packet = NewDataPacket(generator.Network, src, dest, 16, true, func() {})
+	if (generator.Network.Driver.CycleAccurateEventQueue().CurrentCycle % 100 == 0 && generator.Network.Driver.CycleAccurateEventQueue().CurrentCycle < 100000000) {
+		for threadId := 0; threadId < len(generator.TraceFileLines); threadId++ {
+			var currentTraceFileLine = generator.CurrentTraceFileLine[threadId]
 
-		generator.Network.Driver.CycleAccurateEventQueue().Schedule(func() {
-			generator.Network.Receive(packet)
-		}, 1)
+			if currentTraceFileLine < len(generator.TraceFileLines[threadId]) {
+				var src = threadId
+				var dest = len(generator.TraceFileLines) + 1
+
+				var packet = NewDataPacket(generator.Network, src, dest, 16, true, func() {})
+
+				generator.Network.Driver.CycleAccurateEventQueue().Schedule(func() {
+					generator.Network.Receive(packet)
+				}, 1)
+			}
+		}
 	}
 }
